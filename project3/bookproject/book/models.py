4ac98661208bbc7c496ccc3f1ca4f78f70b72826
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2025-07-05 21:38:52 +0900

    feat: Book.__str__の定義をtitleを返すように更新。

--- project3/bookproject/book/models.py
+++ project3/bookproject/book/models.py
@@ -7,3 +7,6 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     category = models.CharField(max_length=100, choices=CATEGORY)
+
+    def __str__(self):
+        return self.title
