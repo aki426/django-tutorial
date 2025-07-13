d066a135fe03184c899e8dc170642b779f66d808
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2025-07-13 21:47:01 +0900

    feat: ログアウト処理を修正。メッセージ表示を追加。

--- project3/bookproject/accounts/views.py
+++ project3/bookproject/accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import logout
+from django.contrib import messages
 
 from .forms import SignupForm
 
@@ -12,3 +14,14 @@ class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
     success_url = reverse_lazy("index")
+
+
+# DjangoのデフォルトLogoutViewがPOSTメソッドのみを受け付けるように設計されているため、
+# 関数ベースビューにてGETメソッドを受け付け即座にログアウトする。
+
+
+def logout_view(request):
+    """即座にログアウト"""
+    logout(request)
+    messages.success(request, "ログアウトしました。")
+    return redirect("index")
