8854d1227eb990d633d4da561e2bc8ca416e01e2
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2025-07-21 18:19:45 +0900

    feat: BookReviewのForm関連ロジックの修正。

--- project3/bookproject/book/views.py
+++ project3/bookproject/book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
@@ -52,7 +52,14 @@ class CreateReviewView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["book"] = Book.objects.get(pk = self.kwargs["book_id"])
+        context["book"] = Book.objects.get(pk=self.kwargs["book_id"])
         print(self.kwargs)
         print(context)
         return context
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("detail-book", kwargs={"pk": self.object.book.id})
