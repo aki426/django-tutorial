5e9e3c5971e0d81bf4fde8391cbefe8f6e8bb097
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2025-07-21 18:49:02 +0900

    feat: サムネイル画像の追加に対応したViewとテンプレート。

--- project3/bookproject/book/views.py
+++ project3/bookproject/book/views.py
@@ -28,7 +28,7 @@ class DetailBookView(DetailView):
 class CreateBookView(CreateView):
     template_name = "book/book_create.html"
     model = Book
-    fields = ("title", "text", "category")
+    fields = ("title", "text", "category", "thumbnail")
     success_url = reverse_lazy("list-book")
 
 
@@ -41,7 +41,7 @@ class DeleteBookView(DeleteView):
 class UpdateBookView(UpdateView):
     template_name = "book/book_update.html"
     model = Book
-    fields = ("title", "text", "category")
+    fields = ("title", "text", "category", "thumbnail")
     success_url = reverse_lazy("list-book")
 
 
