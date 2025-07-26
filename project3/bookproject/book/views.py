fcf08b3dfdab16bd6380cdd184b2be4a56c165dc
金田アキヒロ(KANEDA Akihiro) <aki426@users.noreply.github.com>
2025-07-26 16:38:18 +0900

    feat: Create、Update、DeleteのViewにUserのValidationと対象書籍の作成ユーザでないと実行できないロジックを追加。

--- project3/bookproject/book/views.py
+++ project3/bookproject/book/views.py
@@ -8,6 +8,7 @@ from django.views.generic import (
     UpdateView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from .models import Book, Review
 
 
@@ -34,18 +35,41 @@ class CreateBookView(LoginRequiredMixin, CreateView):
     fields = ("title", "text", "category", "thumbnail")
     success_url = reverse_lazy("list-book")
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        return super().form_valid(form)
+
 
 class DeleteBookView(LoginRequiredMixin, DeleteView):
     template_name = "book/book_confirm_delete.html"
     model = Book
     success_url = reverse_lazy("list-book")
 
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+
+        if obj.user != self.request.user:
+            raise PermissionDenied
+
+        return obj
+
 
 class UpdateBookView(LoginRequiredMixin, UpdateView):
     template_name = "book/book_update.html"
     model = Book
     fields = ("title", "text", "category", "thumbnail")
-    success_url = reverse_lazy("list-book")
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+
+        if obj.user != self.request.user:
+            raise PermissionDenied
+
+        return obj
+
+    def get_success_url(self):
+        return reverse("detail-book", kwargs={"pk": self.object.id})
 
 
 class CreateReviewView(LoginRequiredMixin, CreateView):
